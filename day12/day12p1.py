85924b51d0bb24b33332b751ad5653ae2642c352
xmaggieliu <xmaggie.liu@gmail.com>
2021-12-15 21:34:38 -0500

    day12

---
+++ day12/day12p1.py
@@ -0,0 +1,36 @@
+caves = {}
+used = []
+paths = 0
+
+with open("input.txt", "r") as f:
+    for line in f:
+        begin, end = line.strip().split("-")
+        if begin in caves.keys():
+            caves[begin].append(end)
+        else:
+            caves[begin] = [end]
+        if end in caves.keys():
+            caves[end].append(begin)
+        else:
+            caves[end] = [begin]
+
+def route(cave, used, count):
+    paths = 0
+    for adj in caves[cave]:
+       
+        if adj == "end":
+            paths += 1
+            continue
+        if adj not in used:
+            new_used = []
+            if adj.islower():
+                new_used.append(adj)
+            if adj in caves.keys():
+                paths += route(adj, used[:] + new_used, count + 1)
+
+    return paths
+
+
+paths = route("start", ["start"], 0)
+
+print(paths)
---
+++ day12/day12p2.py
@@ -0,0 +1,44 @@
+caves = {}
+used = []
+paths = 0
+
+with open("input.txt", "r") as f:
+    for line in f:
+        begin, end = line.strip().split("-")
+        if begin in caves.keys():
+            caves[begin].append(end)
+        else:
+            caves[begin] = [end]
+        if end in caves.keys():
+            caves[end].append(begin)
+        else:
+            caves[end] = [begin]
+
+def route(cave, used, count, smalled):
+    paths = 0
+    for adj in caves[cave]:
+        smalls = smalled
+        if adj == "end":
+            paths += 1
+            continue
+        elif adj == "start":
+            continue
+        new_used = []
+        if adj.islower():
+            if smalled and adj in used:
+                continue
+            if adj in used:
+                smalls = True
+            new_used.append(adj)
+        else:
+            new_used.append(adj)
+        if adj in caves.keys():
+            paths += route(adj, used[:] + new_used, count + 1, smalls)
+
+    return paths
+
+
+paths = route("start", ["start"], 0, False)
+
+print(paths)
+
